a2a19dca0ec6df523730e968ccae158e76ba02ae
Mitchell Wilson <mitchellwilson01@outlook.com>
2021-03-15 17:58:40 -0500

    different background image and tinting for home page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <p><i className="fas fa-envelope-open-text"></i> 
             &nbsp;&nbsp;&nbsp;admin@aspen-rentals.com</p>
             <p><i className="fas fa-phone"></i> 
-            &nbsp;&nbsp;&nbsp;979-864-3990</p>
+            &nbsp;&nbsp;&nbsp;(979) 864-3990</p>
             <p><i className="fas fa-map-marker-alt"></i> 
             &nbsp;&nbsp;&nbsp;23603 N. Hwy 288, Angleton, TX 77515</p>
         </div>
