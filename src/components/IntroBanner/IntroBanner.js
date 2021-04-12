46c42e38b114bb3910d230fc473893477873c9b8
Mitchell Wilson <mitchellwilson01@outlook.com>
2021-04-12 16:16:16 -0500

    photos added to about page

--- src/components/IntroBanner/IntroBanner.js
+++ src/components/IntroBanner/IntroBanner.js
@@ -7,7 +7,7 @@ const IntroBanner = () => {
             <div className="logoLeaf"></div>
             <div className="logoText"></div>
             <h3 className="rentals">Briquetter Rentals</h3>
-            <h3>All the savings,<br/>without all the capital</h3>
+            <h3>All the savings, without all the capital</h3>
             <a href="/about"><button >contact</button></a>
         </div>
     );  
--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -6,16 +6,30 @@ const About = () => {
         <div className="about">
             <div className="container">
             <h1>About Aspen</h1>
-            <p>Aspen Industrial Machines, LLC. was created to provide rental 
-            briquetting equipment for the manufacturing and recycling industries.</p>
-            <p>Manufacturing Facilities running machine centers and lathes have an 
-            an ongoing need to handle chips and turnings as well as to recycle 
-            coolant fluids.</p>
-            <p>For years, there have been companies selling briquetter systems. 
-            However there has not been a "zero captial" rental option. Now, there is 
-            a second option that allows you to rent top-of-the-line systems providing 
-            all the value of of briquetting your material without the upfront cost.
-            </p>
+            <div className="contact-info">
+
+            </div>
+            <div className="content">
+                <div className="about">
+                    <p>Aspen Industrial Machines, LLC. was created to provide rental 
+                    briquetting equipment for the manufacturing and recycling industries.</p>
+                </div>
+                <div className="about">
+                    <div className="about1"></div>
+                    <p className="p2">Manufacturing Facilities running machine centers and lathes have an 
+                    an ongoing need to handle chips and turnings as well as to recycle 
+                    coolant fluids.</p>
+                </div>
+                <div className="about about-mad">
+                    <p>For years, there have been companies selling briquetter systems. 
+                    However there has not been a "zero captial" rental option. 
+                    Now, there is a second option that allows you to rent</p>
+                    <p className="p2">top-of-the-line systems providing 
+                    all the value of of briquetting your material without the upfront cost.
+                    </p>
+                    <div className="about2"></div>
+                </div>
+            </div>
             </div>
         </div>
     );
