0a872e0b42fc30d2e36f30d9a3d6dc6f7bf28fd0
Mitchell Wilson <mitchellwilson01@outlook.com>
2021-04-26 00:25:30 -0500

    content added to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.scss';
 
 const Footer = () => {
+    const initialMobile = window.innerWidth < 870 ? true : false;
+    const [mobile, setMobile] = useState(initialMobile);
+
+    const handleResize = () => {
+        if (window.innerWidth < 870) {
+            setMobile(true)
+        } else {
+            setMobile(false);
+        }
+    }
+
+    const logo = <div className="logo"></div>
+    const buttons = <div className="buttons">
+                        <a href="/about"><button className="contact">Contact</button></a>
+                        <a href="/home"><button className="home">Home</button></a>
+                    </div>
+
+    window.addEventListener("resize", handleResize);
+
     return (
         <div className="footer">
+            {mobile ? null : buttons}
+            {mobile ? null : logo}
             <div className="thing">
-            <p><i className="fas fa-user-circle"></i>
-            &nbsp;&nbsp;&nbsp;Christian Martinez</p>
-            <p><i className="fas fa-envelope-open-text"></i> 
-            &nbsp;&nbsp;&nbsp;Christian@aspenindustrialmachines.com</p>
-            <p><i className="fas fa-phone"></i> 
-            &nbsp;&nbsp;&nbsp;(713)-447-1487</p>
+                <p><i className="fas fa-user-circle"></i>
+                &nbsp;&nbsp;&nbsp;Christian Martinez</p>
+                <p><i className="fas fa-envelope-open-text"></i> 
+                &nbsp;&nbsp;&nbsp;Christian@aspenindustrialmachines.com</p>
+                <p><i className="fas fa-phone"></i> 
+                &nbsp;&nbsp;&nbsp;(713)-447-1487</p>
             </div>
         </div>
     );
